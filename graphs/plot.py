8a966178eef95d5308f06b91485f0ba56c234ec9
Sinan Shi <s.shi@ucl.ac.uk>
2016-03-23 12:22:44 +0000

    load data to pandas format

--- graphs/plot.py
+++ graphs/plot.py
@@ -1,60 +1,60 @@
-import numpy as np
+from twitter_graph import twitterData
+from random import random
+from bokeh.charts import TimeSeries
+from bokeh.models import CustomJS, ColumnDataSource
+from bokeh.plotting import figure, output_file, show
 
-from bokeh.models import BoxSelectTool, LassoSelectTool, Paragraph
-from bokeh.plotting import figure, hplot, vplot
+td = twitterData.twitterData("../../twitter_client/twitter_client/data/output_got.csv")
 
-# create three normal population samples with different parameters
-x1 = np.random.normal(loc=5.0, size=400) * 100
-y1 = np.random.normal(loc=10.0, size=400) * 10
+#output_file("callback.html")
 
-x2 = np.random.normal(loc=5.0, size=800) * 50
-y2 = np.random.normal(loc=5.0, size=800) * 10
+TOOLS="resize,pan,wheel_zoom,box_zoom,reset,previewsave"
 
-x3 = np.random.normal(loc=55.0, size=200) * 10
-y3 = np.random.normal(loc=4.0, size=200) * 10
 
-x = np.concatenate((x1, x2, x3))
-y = np.concatenate((y1, y2, y3))
+import pandas as pd
+from bokeh.charts import TimeSeries, output_file, show
 
-TOOLS="pan,wheel_zoom,box_select,lasso_select"
+AAPL = pd.read_csv(
+        "http://ichart.yahoo.com/table.csv?s=AAPL&a=0&b=1&c=2000&d=0&e=1&f=2010",
+        parse_dates=['Date'])
 
-# create the scatter plot
-p = figure(tools=TOOLS, plot_width=600, plot_height=600, title=None, min_border=10, min_border_left=50)
-#r = p.line(x, y, size=3, color="#3A5785", alpha=0.6)
-r = p.line(x, y)
+output_file("timeseries.html")
 
-p.select(BoxSelectTool).select_every_mousemove = False
-p.select(LassoSelectTool).select_every_mousemove = False
+data = dict(AAPL=AAPL['Adj Close'], Date=AAPL['Date'])
 
-# create the horizontal histogram
-#hhist, hedges = np.histogram(x, bins=20)
-#hzeros = np.zeros(len(hedges)-1)
-#hmax = max(hhist)*1.1
+p = TimeSeries(data, title="APPL", ylabel='Stock Prices')
+
+show(p)
+
+
+
+#p.line(data[])
+#p.circle('x', 'y', color='color', size=8, source=s, alpha=0.4)
 #
-#LINE_ARGS = dict(color="#3A5785", line_color=None)
+#s2 = ColumnDataSource(data=dict(ym=[0.5, 0.5]))
+#p.line(x=[0, 1], y='ym', color="orange", line_width=5, alpha=0.6, source=s2)
+##p.text(x=[0, 1], y='ym', text="some label", souce=s1)
 #
-#ph = figure(toolbar_location=None, plot_width=p.plot_width, plot_height=200, x_range=p.x_range,
-#            y_range=(-hmax, hmax), title=None, min_border=10, min_border_left=50)
-#ph.xgrid.grid_line_color = None
-
-def update(attr, old, new):
-    print("something")
-#    inds = np.array(new['1d']['indices'])
-#    if len(inds) == 0 or len(inds) == len(x):
-#        hhist1, hhist2 = hzeros, hzeros
-#        vhist1, vhist2 = vzeros, vzeros
-#    else:
-#        neg_inds = np.ones_like(x, dtype=np.bool)
-#        neg_inds[inds] = False
-#        hhist1, _ = np.histogram(x[inds], bins=hedges)
-#        vhist1, _ = np.histogram(y[inds], bins=vedges)
-#        hhist2, _ = np.histogram(x[neg_inds], bins=hedges)
-#        vhist2, _ = np.histogram(y[neg_inds], bins=vedges)
+#s.callback = CustomJS(args=dict(s2=s2), code="""
+#        var inds = cb_obj.get('selected')['1d'].indices;
+#        var d = cb_obj.get('data');
+#        var ym = 0
 #
-#    hh1.data_source.data["top"]   =  hhist1
-#    hh2.data_source.data["top"]   = -hhist2
-#    vh1.data_source.data["right"] =  vhist1
-#    vh2.data_source.data["right"] = -vhist2
+#        if (inds.length == 0) { return; }
+#
+#        for (i = 0; i < d['color'].length; i++) {
+#            d['color'][i] = "navy"
+#        }
+#        for (i = 0; i < inds.length; i++) {
+#            d['color'][inds[i]] = "firebrick"
+#            ym += d['y'][inds[i]]
+#        }
+#
+#        ym /= inds.length
+#        s2.get('data')['ym'] = [ym, ym]
+#
+#        cb_obj.trigger('change');
+#        s2.trigger('change');
+#    """)
 
-r.data_source.on_change('selected', update)
-
+#show(ts)
